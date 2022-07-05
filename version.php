32a78ee48bb15c4a0c8e73a97c56e598efc7fc2b
Noel De Martin <noel@moodle.com>
2022-07-05 15:40:24 +0200

    Add plugin files
    Check out the commits that caused these changes: https://github.com/moodlehq/moodleapp/compare/c880fe6b9198c41d6e1daf3126405b4627e22cfb...4632eeb932242da4a30780bfc4804fefc1c85dbe

---
+++ version.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * This plugin has been auto-generated, please don't modify it.
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version = 2022070700;
+$plugin->requires = 2016052300;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '4.1.0-dev';
+$plugin->component = 'local_moodleappbehat';
