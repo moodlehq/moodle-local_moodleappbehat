b5c4415ea2d446855568d39b534384e0280e78cb
Pau Ferrer Ocaña <crazyserver@gmail.com>
2022-07-12 12:30:17 +0200

    Get runtime from moodle-local_moodlemobileapp

---
+++ lang/en/local_moodleappbehat.php
@@ -0,0 +1,5 @@
+
+<?php
+
+$string['pluginname'] = 'Moodle App Behat (auto-generated)';
+$string['privacy_metadata'] = 'This plugin should only be used in development environments, and it does not store any user data.';
---
+++ version.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * This plugin has been auto-generated, please don't modify it.
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version = 2022071200;
+$plugin->requires = 2016052300;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '4.0.0';
+$plugin->component = 'local_moodleappbehat';
