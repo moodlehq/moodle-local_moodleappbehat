6fe1d118e4f0f768e51cae4b86197cf56258e363
Dani Palou <dani@moodle.com>
2026-07-14 15:53:29 +0200

    [auto-generated] Update plugin files

--- version.php
+++ version.php
@@ -6,8 +6,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026062900;
+$plugin->version = 2026071400;
 $plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '5.2.0';
+$plugin->release = '5.2.1';
 $plugin->component = 'local_moodleappbehat';
